4baf7e9aa895cdbcb17b75d58650d4fb97e2814e
Levi Lucas <levilucas93@gmail.com>
2017-11-17 19:40:06 +0000

    added in bare bone of video contoleler

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+
+class VideoController extends Controller
+{
+
+    public function post()
+    {
+
+        
+    }
+
+
+}
