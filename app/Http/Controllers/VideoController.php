1301067b334228589dbc76135e665e4de57f1143
Levi Lucas <levilucas93@gmail.com>
2017-11-17 19:41:08 +0000

    added video controler post function following guides

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Http\Request;
+use getID3;
 
 class VideoController extends Controller
 {
 
-    public function post()
+    public function post(Request $videoFile)
     {
 
-        
+        $getID3 = new getID3;
+
+        $this->validate($videoFile, [
+            'video' => 'required'
+        ]);
+
+        $fileInfo = $getID3->analyze($videoFile);
+        return $fileInfo;
     }
-
-
 }
